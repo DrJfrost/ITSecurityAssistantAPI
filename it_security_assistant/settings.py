2c157813bff57700b8d4787d52d43e795a5652c9
DrJfrost <julianrojas99@hotmail.com>
2020-08-08 22:44:12 -0700

    s3 configuration

--- it_security_assistant/settings.py
+++ it_security_assistant/settings.py
@@ -132,7 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # rest framework authentication classes
 REST_FRAMEWORK = {
@@ -150,3 +149,12 @@ AUTH_USER_MODEL = 'users.User'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# S3 storage configuration
+AWS_ACCESS_KEY_ID = 'AKIAYZOTJAY76UQ72W2D'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'it-security-assistant-s3'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
